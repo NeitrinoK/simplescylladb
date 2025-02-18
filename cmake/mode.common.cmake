17dfa9d32e175f1972bee4e19b711887be3ae8a0
NeitrinoK <netrinoph@gmail.com>
2025-02-18 19:35:32 +0300

    Remove clang flags (we use GNU only)

--- cmake/mode.common.cmake
+++ cmake/mode.common.cmake
@@ -90,7 +90,7 @@ if(target_arch)
   add_compile_options("-march=${target_arch}")
 endif()
 
-add_compile_options("SHELL:-Xclang -fexperimental-assignment-tracking=disabled")
+# add_compile_options("SHELL:-Xclang -fexperimental-assignment-tracking=disabled")
 
 function(maybe_limit_stack_usage_in_KB stack_usage_threshold_in_KB config)
   math(EXPR _stack_usage_threshold_in_bytes "${stack_usage_threshold_in_KB} * 1024")
--- configure.py
+++ configure.py
@@ -1853,7 +1853,7 @@ def get_extra_cxxflags(mode, mode_config, cxx, debuginfo):
     # 
     # It seems that we aren't losing much by disabling AssigmentTracking,
     # so for now we choose to disable it to get `coro_frame_ty` back.
-    cxxflags.append('-Xclang -fexperimental-assignment-tracking=disabled')
+    # cxxflags.append('-Xclang -fexperimental-assignment-tracking=disabled')
 
     return cxxflags
 
